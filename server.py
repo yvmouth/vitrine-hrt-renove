0ce030cbf01b4ba3873f702d9a24c2aa5a1a38a8
unknown <charl@LPT-GC.BTNSA.local>
2025-11-19 15:26:40 +0100

    Page contact

--- server.py
+++ server.py
@@ -15,5 +15,9 @@ def services():
 def apropos():
     return render_template("apropos.html")
 
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
